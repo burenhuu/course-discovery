8a27bdfcfaf998f5a66fc4763a80f446d6a55cd4
burenhuu <47760847+burenhuu@users.noreply.github.com>
2023-11-05 21:02:28 +0800

    Update serializers.py

--- course_discovery/apps/api/serializers.py
+++ course_discovery/apps/api/serializers.py
@@ -1811,7 +1811,7 @@ class MinimalProgramSerializer(TaggitSerializer, FlexFieldsSerializerMixin, Base
         model = Program
         fields = (
             'uuid', 'title', 'subtitle', 'type', 'type_attrs', 'status', 'marketing_slug', 'marketing_url',
-            'banner_image', 'hidden', 'courses', 'authoring_organizations', 'card_image_url',
+            'banner_image', 'banner_image_url', 'hidden', 'courses', 'authoring_organizations', 'card_image_url',
             'is_program_eligible_for_one_click_purchase', 'degree', 'curricula', 'marketing_hook',
             'total_hours_of_effort', 'recent_enrollment_count', 'organization_short_code_override',
             'organization_logo_override_url', 'primary_subject_override', 'level_type_override', 'language_override',
